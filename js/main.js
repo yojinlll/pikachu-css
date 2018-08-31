7f84d543434fbbdaa7d7d85c09651df93efe47ed
yojinlll <yojin1995@qq.com>
2018-08-31 22:38:22 +0800

    皮卡丘-调速版

--- js/main.js
+++ js/main.js
@@ -1,18 +1,40 @@
 !function(){
+    var duration = 20
+    $('.actions').on('click','button',(e)=>{
+        let $button = $(e.currentTarget)
+        let speed = $button.attr('data-speed')
+        console.log(speed)
+        $button.addClass('active')
+            .siblings('.active').removeClass('active')
+        switch(speed){
+            case'slow':
+                duration = 50
+                break
+            case'normal':
+                duration = 30
+                break
+            case'fast':
+                duration = 10
+                break
+        }
+    })
+
     function writeCode(prefix,code,fn){
         let container = document.querySelector('#code')
         let styleTag = document.querySelector('#styleTag')
         let n = 0
-        let id =  setInterval(()=>{
+        let id
+        id =  setTimeout(function run(){
             n += 1
             container.innerHTML = code.substring(0,n)
             styleTag.innerHTML = code.substring(0,n)
             container.scrollTop = container.scrollHeight
-            if( n >= code.length){
-                window.clearInterval(id)
+            if( n < code.length){
+                id = setTimeout(run,duration)
+            }else{
                 fn && fn.call()
             }
-        },10)
+        },duration)
     }
     let code = 
 `
@@ -145,4 +167,5 @@
     }
 `
     writeCode('',code)
+
 }.call()
